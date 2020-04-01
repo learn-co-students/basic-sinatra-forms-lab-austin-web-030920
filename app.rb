1c247ca50b013d2925fa843f4767b113eac90781
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-03-31 22:07:52 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,23 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
+
+    post "/newteam" do
+        #puts params
+       
+        @team_name = params[:name]
+        @coach = params[:coach]
+        @p_guard = params[:p_guard]
+        @s_guard = params[:s_guard]
+        @s_forward = params[:s_forward]
+        @p_forward = params[:p_forward]
+        @center = params[:center]
+        erb = :team
+    end
+
 
 end
+
